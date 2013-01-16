93e56749e477b182ea70547cccf59618359615e3
thousandsofthem <thousandsofthem@gmail.com>
2013-01-16 04:02:42 +0300

    Added Composer support; bump version

--- IronCore.class.php
+++ IronCore.class.php
@@ -5,13 +5,13 @@
  * @link https://github.com/iron-io/iron_core_php
  * @link http://www.iron.io/
  * @link http://dev.iron.io/
- * @version 0.1.1
+ * @version 0.1.2
  * @package IronCore
  * @copyright BSD 2-Clause License. See LICENSE file.
  */
 
 class IronCore{
-    protected $core_version = '0.1.1';
+    protected $core_version = '0.1.2';
 
     // should be overridden by child class
     protected $client_version = null;
