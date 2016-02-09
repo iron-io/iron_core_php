cd70e20d296158b53af38f5c35f13f661e7abb5e
Andrew Minerd <andrew@parlevelsystems.com>
2016-02-09 17:32:34 -0600

    remove EOF check before retry on server error

--- IronCore.class.php
+++ IronCore.class.php
@@ -335,12 +335,6 @@ class IronCore
                 case self::HTTP_ACCEPTED:
                     return $_out;
                 case Http_Exception::INTERNAL_ERROR:
-                    if (strpos($_out, "EOF") !== false) {
-                        self::waitRandomInterval($retry);
-                    } else {
-                        $this->reportHttpError($this->last_status, $_out);
-                    }
-                    break;
                 case Http_Exception::SERVICE_UNAVAILABLE:
                 case Http_Exception::GATEWAY_TIMEOUT:
                     self::waitRandomInterval($retry);
